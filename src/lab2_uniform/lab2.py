c9e276fa17a775dd002c74e14bbe94389bd6c422
YujiePan <panyujie97@126.com>
2018-05-30 14:56:47 +0800

    init

--- src/lab2_uniform/lab2.py
+++ src/lab2_uniform/lab2.py
@@ -2,7 +2,7 @@
 # @Author: Yujie Pan
 # @Date:   2018-05-30 11:37:19
 # @Last Modified by:   Yujie Pan
-# @Last Modified time: 2018-05-30 14:35:25
+# @Last Modified time: 2018-05-30 14:53:01
 
 
 import sqlite3
@@ -11,10 +11,11 @@ import math
 import sys
 import time
 import random
+import datetime
 
 SIZE_DB = [0, 32929106, 33036408, 32872161, 32784690]
 MIX_SIZE = 0
-SIMU_TIMES = 250
+SIMU_TIMES = 20
 
 
 def main():
@@ -25,12 +26,12 @@ def main():
     print("MIXIN: %d")
     info = simulate(i, j, SIZE_DB[i],  SIMU_TIMES)
     append_to_file(
-        i, "../../result/lab3-th{}-mix{}.txt".format(i, MIX_SIZE), info)
+        i, "../../result/lab2-th{}-mix{}.txt".format(i, MIX_SIZE), info)
 
 
 def append_to_file(i, name, content):
     with open(name, "a") as f:
-        f.write("[{}]TH{}:\n{}\r\n".format(time.time(), i, content))
+        f.write("[{}] TH{}: {}\r\n".format(timestamp2string(time.time()), i, content))
 
 
 def rand_choose_from_db(cur, random_num):
@@ -58,8 +59,8 @@ def mini_tx_id_2_block(cur, mini_id):
 def disp_progress(start_time, start_p, now_p, end_p, comment):
     try:
         print("=" * 20, "\nREADY:\t", comment)
-        pct = (now_p - start_p) / (end_p - start_p)
-        used_time = time.time() - start_time
+        pct=(now_p - start_p) / (end_p - start_p)
+        used_time=time.time() - start_time
         print("COST TIME:\t", used_time // 60, ' min\t= ', used_time, " s")
         print("REMAINING:\t", used_time / (pct + 1e-10) * (1 - pct + 1e-10) //
               60, ' min\t= ', used_time / (pct + 1e-10) * (1 - pct + 1e-10), " s")
@@ -68,53 +69,54 @@ def disp_progress(start_time, start_p, now_p, end_p, comment):
 
 
 def simulate(thread_id, MIX, max_random,  SIMU_TIMES):
-    t1 = time.time()  # get current time
+    t1=time.time()  # get current time
 
-    DB_TX_IN_OUT_PATH = "../../db/new_tx_in_out{}.sqlite".format(thread_id)
-    DB_TX_BL_PATH = "../../db/new_tx_block.sqlite"
+    DB_TX_IN_OUT_PATH="../../db/new_tx_in_out{}.sqlite".format(thread_id)
+    DB_TX_BL_PATH="../../db/new_tx_block.sqlite"
 
-    pool_size = int((MIX + 1) * 1.5 + 1)
-    pool_size = MIX
+    pool_size=int((MIX + 1) * 1.5 + 1)
+    pool_size=MIX
     # to simplify
 
-    db_record = sqlite3.connect(DB_TX_IN_OUT_PATH).cursor()
-    db_find_block = sqlite3.connect(DB_TX_BL_PATH).cursor()
+    db_record=sqlite3.connect(DB_TX_IN_OUT_PATH).cursor()
+    db_find_block=sqlite3.connect(DB_TX_BL_PATH).cursor()
 
-    guess_times = [0 for i in range(MIX + 2)]
+    guess_times=[0 for i in range(MIX + 2)]
 
     for k in range(SIMU_TIMES):
         if int(k % 10) == int(0):
             disp_progress(t1, 0, k, SIMU_TIMES, str(k))
 
-        selected_mini = [0 for jj in range(pool_size + 1)]
+        selected_mini=[0 for jj in range(pool_size + 1)]
 
-        now_tx, real_out, real_out_n = rand_choose_from_db(
+        now_tx, real_out, real_out_n=rand_choose_from_db(
             db_record, random.randint(3, SIZE_DB[thread_id] - 1))
         while now_tx == 0:
-            now_tx, real_out, real_out_n = rand_choose_from_db(
+            now_tx, real_out, real_out_n=rand_choose_from_db(
                 db_record, random.randint(3, SIZE_DB[thread_id] - 1))
 
-        xxx, real_mini_id = mini_tx_id(db_find_block, real_out, real_out_n)
-        xxx, now_mini_id = mini_tx_id(db_find_block, now_tx, 0)
+        xxx, real_mini_id=mini_tx_id(db_find_block, real_out, real_out_n)
+        xxx, now_mini_id=mini_tx_id(db_find_block, now_tx, 0)
 
         print("TH", thread_id, "Random Choose:", now_tx, "from",
               real_out, real_out_n, "mini_id", real_mini_id)
-        #row_prob[0] = 1
-        selected_mini[0] = real_mini_id
+        # row_prob[0] = 1
+        selected_mini[0]=real_mini_id
 
         for j in range(pool_size):
-            sample = random.randint(1, now_mini_id - 1)
+            sample=random.randint(1, now_mini_id - 1)
             while sample < 100 or sample in selected_mini:
-                sample = radom.randint(1, now_mini_id - 1)
-            selected_mini[j + 1] = sample
+                sample=radom.randint(1, now_mini_id - 1)
+            selected_mini[j + 1]=sample
             print("\tTH", thread_id, "choose_miniid:", selected_mini)
-            selected_mini.sort()
-            guess_time = find_index(selected_mini, real_mini_id, MIX)
-            guess_times[MIX + 1 - guess_time] += 1
-            print("\tTH", thread_id, "final:", now_tx, "from", real_out,
-                  real_out_n, "guess time:", MIX + 1 - guess_time)
+        selected_mini.sort()
+        guess_time=find_index(selected_mini, real_mini_id, MIX)
+        guess_times[MIX + 1 - guess_time] += 1
+        print("\tTH", thread_id, "final:", now_tx, "from", real_out,
+              real_out_n, "guess time:", MIX + 1 - guess_time)
 
-    info = "OK: {}".format(guess_times)
+    info="OK: {}".format(str(guess_times))
+    return info
 
 
 def is_in_array(arr, element, start, end):
@@ -128,6 +130,18 @@ def find_index(arr, element, largest):
     for i in range(largest, -1, -1):
         if arr[i] == element:
             return i
+    print("-1")
     return -1
+
+
+def timestamp2string(timeStamp):
+    try:
+        d=datetime.datetime.fromtimestamp(timeStamp)
+        str1=d.strftime("%Y-%m-%d-%H-%M-%S.%f")
+        # 2015-08-28 16:43:37.283000'
+        return str1
+    except Exception as e:
+        print(e)
+        return ''
 
 main()
