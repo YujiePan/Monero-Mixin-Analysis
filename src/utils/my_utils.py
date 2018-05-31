362eae060fb648eab05c543ace712118551bfff6
YujiePan <panyujie97@126.com>
2018-05-31 10:47:52 +0800

    useless

--- src/utils/my_utils.py
+++ src/utils/my_utils.py
@@ -4,9 +4,10 @@
 # @Last Modified by:   pyj
 # @Last Modified time: 2018-05-27 05:54:22
 
-from scipy.special import gamma
-from scipy import integrate
+
 import numpy as np
+from scipy.stats import gamma
+import matplotlib.pyplot as  plt
 
 def gamma_pdf(x, alpha, beta):
     return beta**alpha * x**(alpha - 1) * np.exp(-beta * x) / gamma(alpha)
@@ -14,3 +15,12 @@ def gamma_pdf(x, alpha, beta):
 
 def gamma_cdf(x, alpha, beta):
     return [integrate.quad(gamma_pdf, 0, t, args=(alpha, beta)) for t in x]
+
+
+ALPHA = 19.28
+BETA = 1.61
+D = gamma(ALPHA, scale=1/BETA)
+
+x = np.linspace(0,20,1000)
+plt.plot(x, D.pdf(x))
+plt.show()
